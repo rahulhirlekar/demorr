93b9818c3efa7e5aa7bd0fcc022bc6b2c9bb8500
RahulHirlekar <rahulhirlekar798@gmail.com>
2023-08-02 14:02:50 +0530

    Initial commit

---
+++ src/practice.java
@@ -0,0 +1,13 @@
+public class practice    {
+
+          int a = 10;
+
+          int b = 10;
+
+      public void sum() {
+
+              int c = a+b;
+          System.out.print("sum = " + c);
+
+              }
+          }
